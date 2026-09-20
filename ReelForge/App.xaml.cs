b28e5f8c6987b98b3d2285545b6bf559ce23e32d
sk225649625-sys <sk225649625@gmail.com>
2026-09-20 23:35:49 +0530

    Delete ReelForge directory

--- ReelForge/App.xaml.cs
+++
@@ -1,7 +0,0 @@
-using System.Windows;
-
-namespace ReelForge;
-
-public partial class App : Application
-{
-}
--- ReelForge/GlobalUsings.cs
+++
@@ -1,4 +0,0 @@
-global using System;
-global using System.IO;
-global using System.Linq;
-global using System.Collections.Generic;
